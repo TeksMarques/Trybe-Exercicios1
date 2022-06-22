03a2c311827a708a08c8754d3cf21ca6b2c54f6e
Teresa Marques <taotaodoce@gmail.com>
2022-06-22 16:19:11 -0300

    assunto concluido

--- fundamentos/bloco04-introducao-javascript-e-logica-de-programcao/for.js
+++ fundamentos/bloco04-introducao-javascript-e-logica-de-programcao/for.js
@@ -21,3 +21,41 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for (let index =0;index < groceryList.length;index+=1) {
     console.log(groceryList[index]);
 }
+
+// uso do for/of
+
+let numeros = [1,2,3,4,5];
+for(let numero of numeros) {
+  console.log(numero);
+}
+
+// onde o "of" vai mostrar as propriedades;
+
+
+let word = 'Hello';
+for (let letter of word) {
+  console.log(letter);
+}
+// resultado:
+// "H"
+// "e"
+// "l"
+// "l"
+// "o"
+
+// e tambem permite somar um valor a cada elemento, no caso 1
+
+let arrOfNumbers = [10, 20, 30];
+for (let sum of arrOfNumbers) {
+  sum += 1;
+  console.log(sum);
+}
+// 11
+// 21
+// 31
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let nome of names) {
+    console.log(nome)
+}
