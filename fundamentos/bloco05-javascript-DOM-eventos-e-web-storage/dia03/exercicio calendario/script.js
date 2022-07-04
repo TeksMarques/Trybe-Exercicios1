12d3f747b11a4c40785b381946aaf7ea77b39832
Teresa Marques <taotaodoce@gmail.com>
2022-07-04 12:26:39 -0300

    exercicio calendario concluido ate o item 10

--- fundamentos/bloco05-javascript-DOM-eventos-e-web-storage/dia03/exercicio calendario/script.js
+++ fundamentos/bloco05-javascript-DOM-eventos-e-web-storage/dia03/exercicio calendario/script.js
@@ -52,3 +52,127 @@ function botaoFeriado(nomeBotao) {
 }
 
 botaoFeriado('Feriados')
+
+function fundoFeriados() {
+    let btnFeriado = document.querySelector('#btn-holiday');
+    let feriados = document.querySelectorAll('.holiday')
+    let backgroundColor = 'rgb(238,238,238)';
+    let setNewColor = 'white';
+
+    btnFeriado.addEventListener('click', function () {
+        for (let index = 0; index < feriados.length; index += 1) {
+            if (feriados[index].style.backgroundColor === setNewColor) {
+                feriados[index].style.backgroundColor = backgroundColor;
+            } else {
+                feriados[index].style.backgroundColor = setNewColor;
+            }
+        }
+    });
+}
+
+fundoFeriados();
+
+function botaoSexta(nomeBotao) {
+    let buttonContainer = document.querySelector('.buttons-container');
+    let novoBotao = document.createElement('button');
+    novoBotao.id = 'btn-friday'
+    novoBotao.innerHTML = nomeBotao
+    buttonContainer.appendChild(novoBotao);
+}
+
+botaoSexta('Sexta-feira')
+
+function textoSexta(sextaArray) {
+    let sextaBotao = document.querySelector('#btn-friday');
+    let sextas = document.getElementsByClassName('friday');
+    let novoTexto = 'Sextou o/'
+
+    sextaBotao.addEventListener('click', function () {
+        for (let index = 0; index < sextas.length; index += 1) {
+            if (sextas[index].innerHTML !== novoTexto) {
+                sextas[index].innerHTML = novoTexto
+            } else {
+                sextas[index].innerHTML = sextaArray[index]
+            }
+        }
+    })
+}
+
+let sextasDezembro = [4, 11, 18, 25]
+textoSexta(sextasDezembro)
+
+function diaMouseOver() {
+    let dias = document.querySelector('#days')
+    dias.addEventListener('mouseover', function (event) {
+        event.target.style.fontSize = '30px';
+        event.target.style.fontWeight = '600'
+    })
+}
+
+function diaMouseOut() {
+    let dias = document.querySelector('#days')
+    dias.addEventListener('mouseout', function (event) {
+        event.target.style.fontSize = '20px'
+        event.target.style.fontWeight = '300'
+    })
+}
+
+diaMouseOver()
+diaMouseOut()
+
+function novaTarefa(tarefas) {
+    let tarefasContainer = document.querySelector('.my-tasks');
+    let tarefasNome = document.createElement('span');
+    tarefasNome.innerHTML = tarefas;
+    tarefasContainer.appendChild(tarefasNome);
+}
+
+novaTarefa('Projeto');
+
+function novaTafDiv(cor) {
+    let tafContainer = document.querySelector('.my-tasks');
+    let novaTaf = document.createElement('div');
+    novaTaf.className = 'task';
+    novaTaf.style.backgroundColor = cor;
+    tafContainer.appendChild(novaTaf)
+}
+
+novaTafDiv('pink')
+
+function selecionarTarefaClass() {
+    let selecionarTarefa = document.getElementsByClassName('task selected');
+    let minhasTarefas = document.querySelector('.task');
+    minhasTarefas.addEventListener('click', function (event) {
+        if (selecionarTarefa.length === 0) {
+            event.target.className = 'task selected'
+        } else {
+            event.target.className = 'task'
+        }
+    })
+}
+
+selecionarTarefaClass()
+
+function corFundoTarefa(){
+    let selecionarTarefa = document.getElementsByClassName('task selected');
+    let dias = document.querySelector('#days');
+    let tafDiv = document.querySelector('.task');
+    let tafCor = tafDiv.style.backgroundColor;
+
+    dias.addEventListener('click', function(event){
+        let eventTargetCor = event.target.style.color;
+        if(selecionarTarefa.length > 0 && eventTargetCor !== tafCor){
+            let color = selecionarTarefa[0].style.backgroundColor;
+            event.target.style.color = color
+        }else if(eventTargetCor === tafCor){
+            event.target.style.color = 'rgb(119,119,119)';
+        }            
+    })
+}
+
+corFundoTarefa()
+
+
+
+
+
